84e9b8a0be7e591b9e0f96eee3a14a00463e29d4
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2024-05-30 14:00:53 -0300

    config: configuração eslint

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.js"],
+    ignores: ["node_modules", "dist", "/*.js"],
+    rules: {
+      semi: "error",
+      "prefer-const": "warn",
+    },
+  },
+  {
+    files: ["**/*.ts"],
+    ignores: ["**/mocks/*.ts"],
+    rules: {
+      "prefer-const": "warn",
+    },
+  },
+];
