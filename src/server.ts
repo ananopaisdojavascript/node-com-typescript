77383e829e483c4fc5ffc3ba6f5280c41e0de50f
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2024-05-30 14:28:00 -0300

    config: configuração prettier e prisma

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import express from 'express'
-const app = express()
-const port = 3000
+import express from "express";
+const app = express();
+const port = 3000;
 
-app.get('/', (_req, res) => {
-  res.send('Hello Cutie!')
-})
+app.get("/", (_req, res) => {
+  res.send("Hello Cutie!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
